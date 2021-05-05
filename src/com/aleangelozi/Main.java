18430aa8a1952f127c71bac5134cacabe8173a5b
aleangelozi <aleangelozi@gmail.com>
2021-05-05 10:40:17 -0300

    Initial commit

---
+++ src/com/aleangelozi/Main.java
@@ -0,0 +1,12 @@
+package com.aleangelozi;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String message = "Welcome to this course ";
+
+        System.out.println(message  + "Alessandro!");
+        System.out.println(message +"Josh!");
+
+    }
+}
